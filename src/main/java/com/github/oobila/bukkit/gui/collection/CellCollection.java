a909e72e5bef1b4c34d319dbfa2328b352cb2ec0
BuildTools <unconfigured@null.spigotmc.org>
2024-07-12 18:17:50 +0100

    fixing cells

--- src/main/java/com/github/oobila/bukkit/gui/collection/CellCollection.java
+++ src/main/java/com/github/oobila/bukkit/gui/collection/CellCollection.java
@@ -31,7 +31,6 @@ public class CellCollection implements CellCollectionInterface {
 
     public CellCollection(int allocatedSize) {
         this.allocatedSize = allocatedSize;
-        //will add later
     }
 
     public CellCollection(List<GuiCell> cells) {
